66f486a4c4dcd009b6751ddad05ee477744398a7
andrey <andrey.iv.cherkessk@gmail.com>
2022-06-22 09:38:17 +0300

    ...

--- app/shop/tests.py
+++ app/shop/tests.py
@@ -1,3 +1,14 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from .lbs import clean_string
+
+class CleanStringTestCase(TestCase):
+
+	dirty_s = '~`а!@#н$%^д&*(р)-_+=/*-+/?.>,<\"е\';:[{] }.й!|\\'
+	clean_s = ''
+
+	def setUp(self):
+		self.clean_s = clean_string(self.dirty_s)
+
+	def test_string(self):
+		self.assertEqual(self.clean_s, 'a-n-d-r-e-j') 
